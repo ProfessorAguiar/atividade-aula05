a95b636786216e41900c13904e1bb6581edb9091
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-07 08:12:46 -0300

    Aula 06-03

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -1,8 +1,12 @@
+import Tarefa from '../models/Tarefa.js'
 function tarefaController(app) {
     app.get('/tarefa', listar)
     app.post('/tarefa', inserir)
     function listar(req, res) {
-        res.send('Rota ativada com GET e recurso tarefa: lista de tarefas deve ser retornada')
+        const d = new Date();
+        const tarefa=new Tarefa('Back-End','uso do Express e bibliotecas',true,d)
+        res.send('Rota ativada com GET e recurso tarefa:')
+        console.log(tarefa)
     }
     function inserir(req, res) {
         res.send('Rota ativada com POST e recurso tarefa: tarefa deve ser inserida')
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,8 +1,11 @@
+import Usuario from '../models/Usuario.js'
 function usuarioController(app) {
     app.get('/usuario', listar)
     app.post('/usuario', inserir)
     function listar(req, res) {
-        res.send('Rota ativada com GET e recurso usuario: lista de usuarios deve ser retornada')
+        const user1=new Usuario('Vinicius Aguiar','aguiar@senac.com','123456')
+        res.send('Rota ativada com GET e recurso usuario:')
+        console.log(user1)
     }
     function inserir(req, res) {
         res.send({
