d0c3761db603425decc20e48bfb3440a65a3ec6f
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-03-08 10:55:39 -0300

    Atualização final da Aula 9
    Métodos GET, POST, PUT e DELETE implementados

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -4,14 +4,16 @@ function usuarioController(app) {
     app.get('/usuario/email/:email', buscarPorEmail)
     app.post('/usuario', inserir)
     app.delete('/usuario/email/:email', deletar)
+    app.put('/usuario/email/:email', atualizar)
     function listar(req, res) {
         const usuarios = bdUsuarios
         // Devolve a lista de Usuarios
         res.send(usuarios)
     }
     function inserir(req, res) {
-        res.send('Método POST')
-        console.log(req.body)
+        res.send('Usuário inserido com sucesso')
+        const userAdd=req.body
+        bdUsuarios.push(userAdd)
     }
     function buscarPorEmail(req, res){
         // Busca o email na lista de usuarios
@@ -36,6 +38,20 @@ function usuarioController(app) {
         bdUsuarios.splice(index, 1)
         // Devolve o usuario deletado
         res.send(usuario)
+    }
+    function atualizar(req, res){
+        // Busca o email na lista de usuarios
+        const usuario = bdUsuarios.find(usuario => usuario.email === req.params.email)
+        // Se o usuario não for encontrado, devolve um erro
+        if(!usuario){
+            res.status(404).send('Usuário não encontrado')
+        }
+        // Se o usuario for encontrado, deleta o usuario
+        usuario.nome = (req.body.nome || usuario.nome)
+        usuario.email = (req.body.email || usuario.email)
+        usuario.senha = (req.body.senha || usuario.senha)
+        // Devolve o usuario atualizado
+        res.send({mensagem: 'Usuário alterado com sucesso'})
     }
 }
 export default usuarioController
